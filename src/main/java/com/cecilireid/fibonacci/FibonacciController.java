ad4c72a8da0940b12b83ee8dbfcdc4a5e10ea070
Cecili Reid <creid31@users.noreply.github.com>
2021-06-05 12:24:43 -0400

    01_01 setup

---
+++ src/main/java/com/cecilireid/fibonacci/FibonacciController.java
@@ -0,0 +1,30 @@
+package com.cecilireid.fibonacci;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("fibonacci")
+public class FibonacciController {
+
+    /**
+     * Determine the n-th fibonacci number
+     *
+     * @param n position of the fibonacci number requested
+     * @return the n-th fibonacci number in the sequence
+     */
+    @GetMapping("findNumber")
+    public ResponseEntity<Integer> findFibonacciNumber(@RequestParam int n) {
+        return ResponseEntity.ok(fibonacci(n));
+    }
+
+    /**
+     * Recursively find the Fibonacci number at the position in the sequence
+     *
+     * @param position requested position of fibonacci sequence (i.e. 8th number in sequence)
+     * @return fibonacci number at position (i.e. 21)
+     */
+    private int fibonacci(int position) {
+        return fibonacci(position - 1) + fibonacci(position - 2);
+    }
+}
---
+++ src/test/java/com/cecilireid/fibonacci/FibonacciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cecilireid.fibonacci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FibonacciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
