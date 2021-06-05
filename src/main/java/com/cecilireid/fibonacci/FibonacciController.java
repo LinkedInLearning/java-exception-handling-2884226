bde83ba4c1ff6b63fea9aa66a896b0955270696a
Cecili Reid <creid31@users.noreply.github.com>
2021-06-05 13:09:41 -0400

    Add more endpoints for fibonacci sequence

--- src/main/java/com/cecilireid/fibonacci/FibonacciController.java
+++ src/main/java/com/cecilireid/fibonacci/FibonacciController.java
@@ -3,9 +3,14 @@ package com.cecilireid.fibonacci;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("fibonacci")
 public class FibonacciController {
+
     /**
      * Determine the n-th fibonacci number
      *
@@ -13,9 +18,56 @@ public class FibonacciController {
      * @return the n-th fibonacci number in the sequence
      */
     @GetMapping("findNumber")
-    public ResponseEntity<Integer> findFibonacciNumber(@RequestParam int n)
-    {
+    public ResponseEntity<Integer> findFibonacciNumber(@RequestParam int n) {
         return ResponseEntity.ok(fibonacci(n));
+    }
+
+
+
+    /**
+     * Store the first n-th numbers in the fibonacci sequence in a text file.
+     *
+     * @param n position in fibonacci sequence
+     * @return name of the file created
+     */
+    @PostMapping("createSequence")
+    public ResponseEntity<String> generateFibonacciSequence(@RequestParam int n) {
+        List<Integer> sequence = getSequence(n);
+        return ResponseEntity.ok(storeSequence(sequence));
+    }
+
+    /**
+     * Generate fibonacci sequence without using recursion
+     *
+     * @param n number of numbers that should be included in the fibonacci sequence
+     * @return list of integers with fibonacci sequence
+     */
+    private List<Integer> getSequence(int n) {
+        List<Integer> sequence = new ArrayList<>();
+        sequence.add(0);
+        int prev = 0;
+        int curr = 1;
+        int index = 1;
+        while (index <= n) {
+            sequence.add(curr);
+            int next = prev + curr;
+            prev = curr;
+            curr = next;
+            index++;
+        }
+        return sequence;
+    }
+
+    /**
+     * Save the fibonacci sequence in a txt file
+     *
+     * @param sequence list of ints in the fibonacci sequence
+     * @return String name of the file saved
+     */
+    private String storeSequence(List<Integer> sequence) {
+        String name = "fibonacci.txt";
+        // TODO
+        return name;
     }
 
     /**
@@ -24,8 +76,7 @@ public class FibonacciController {
      * @param position requested position of fibonacci sequence (i.e. 8th number in sequence)
      * @return fibonacci number at position (i.e. 21)
      */
-    private int fibonacci(int position)
-    {
+    private int fibonacci(int position) {
         if (position <= 1) {
             return position;
         }
